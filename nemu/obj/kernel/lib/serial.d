0994d05de461b275891eaa92db1bd4aa51774f84
tracer-NEMU2020 <tjuics2020@163.com>
2023-04-27 17:16:38 +0800

    >  compile kernel 3018216336 rand Linux rand 5.15.0-67-generic #74~20.04.1-Ubuntu SMP Wed Feb 22 14:52:34 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:16:38 up 2 days,  6:12,  1 user,  load average: 1.84, 1.53, 1.63 17267f915b40038aea2ef75c11cc079580c49873

---
+++ nemu/obj/kernel/lib/serial.d
@@ -0,0 +1,25 @@
+obj/kernel/lib/serial.o: kernel/src/lib/serial.c kernel/include/common.h \
+ lib-common/trap.h lib-common/uclibc/include/stdint.h \
+ lib-common/uclibc/include/features.h \
+ lib-common/uclibc/include/bits/uClibc_config.h \
+ lib-common/uclibc/include/sys/cdefs.h \
+ lib-common/uclibc/include/bits/wordsize.h \
+ lib-common/uclibc/include/sys/types.h \
+ lib-common/uclibc/include/bits/types.h \
+ lib-common/uclibc/include/bits/typesizes.h \
+ lib-common/uclibc/include/bits/pthreadtypes.h \
+ lib-common/uclibc/include/bits/sched.h lib-common/uclibc/include/time.h \
+ lib-common/uclibc/include/endian.h \
+ lib-common/uclibc/include/bits/endian.h \
+ lib-common/uclibc/include/byteswap.h \
+ lib-common/uclibc/include/bits/byteswap.h \
+ lib-common/uclibc/include/bits/byteswap-common.h \
+ lib-common/uclibc/include/sys/select.h \
+ lib-common/uclibc/include/bits/select.h \
+ lib-common/uclibc/include/bits/sigset.h \
+ lib-common/uclibc/include/bits/time.h \
+ lib-common/uclibc/include/sys/sysmacros.h kernel/include/debug.h \
+ kernel/include/common.h kernel/include/x86.h kernel/include/x86/cpu.h \
+ lib-common/x86-inc/cpu.h kernel/include/x86/io.h \
+ kernel/include/x86/memory.h lib-common/x86-inc/mmu.h \
+ kernel/include/x86.h
