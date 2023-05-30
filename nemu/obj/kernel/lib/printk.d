b6dc27267b2f2a6142dbca1b95a75be70b18478d
tracer-NEMU2020 <tjuics2020@163.com>
2023-05-30 15:05:48 +0800

    >  compile kernel 3018216336 rand Linux rand 5.15.0-71-generic #78-Ubuntu SMP Tue Apr 18 09:00:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:05:48 up 12 days,  3:20,  4 users,  load average: 0.61, 0.78, 0.81 44920b4f9b8702e36230c0107770617e9db0f15d

---
+++ nemu/obj/kernel/lib/printk.d
@@ -0,0 +1,34 @@
+obj/kernel/lib/printk.o: kernel/src/lib/printk.c kernel/include/common.h \
+ lib-common/trap.h lib-common/uclibc/include/stdint.h \
+ lib-common/uclibc/include/features.h \
+ lib-common/uclibc/include/bits/uClibc_config.h \
+ lib-common/uclibc/include/sys/cdefs.h \
+ lib-common/uclibc/include/bits/wordsize.h \
+ lib-common/uclibc/include/sys/types.h \
+ lib-common/uclibc/include/bits/types.h \
+ lib-common/uclibc/include/bits/typesizes.h \
+ lib-common/uclibc/include/bits/pthreadtypes.h \
+ lib-common/uclibc/include/bits/sched.h lib-common/uclibc/include/time.h \
+ lib-common/uclibc/include/endian.h \
+ lib-common/uclibc/include/bits/endian.h \
+ lib-common/uclibc/include/byteswap.h \
+ lib-common/uclibc/include/bits/byteswap.h \
+ lib-common/uclibc/include/bits/byteswap-common.h \
+ lib-common/uclibc/include/sys/select.h \
+ lib-common/uclibc/include/bits/select.h \
+ lib-common/uclibc/include/bits/sigset.h \
+ lib-common/uclibc/include/bits/time.h \
+ lib-common/uclibc/include/sys/sysmacros.h kernel/include/debug.h \
+ kernel/include/common.h kernel/include/x86.h kernel/include/x86/cpu.h \
+ lib-common/x86-inc/cpu.h kernel/include/x86/io.h \
+ kernel/include/x86/memory.h lib-common/x86-inc/mmu.h \
+ lib-common/uclibc/include/stdio.h \
+ lib-common/uclibc/include/bits/uClibc_stdio.h \
+ lib-common/uclibc/include/bits/uClibc_mutex.h \
+ lib-common/uclibc/include/pthread.h lib-common/uclibc/include/sched.h \
+ lib-common/uclibc/include/bits/uClibc_clk_tck.h \
+ lib-common/uclibc/include/signal.h \
+ lib-common/uclibc/include/bits/initspin.h \
+ lib-common/uclibc/include/bits/sigthread.h \
+ lib-common/uclibc/include/bits/uClibc_pthread.h \
+ lib-common/uclibc/include/bits/stdio_lim.h
