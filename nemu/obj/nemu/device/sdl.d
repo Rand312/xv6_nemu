e1fbad9e5c59e89f55f78630eceab0c2fa82848c
pengzejie <pengzejie@bytedance.com>
2023-05-30 15:18:54 +0800

    keyboard, fix 通码没有触发中断的bug, 临时解决方案

--- nemu/obj/nemu/device/sdl.d
+++
@@ -1,4 +0,0 @@
-obj/nemu/device/sdl.o: nemu/src/device/sdl.c nemu/include/common.h \
- nemu/include/debug.h nemu/include/macro.h nemu/include/cpu/reg.h \
- nemu/include/cpu/../../../lib-common/x86-inc/cpu.h nemu/src/device/sdl.h \
- nemu/src/device/vga.h
--- nemu/obj/nemu/monitor/hashmap.d
+++
@@ -1,1 +0,0 @@
-obj/nemu/monitor/hashmap.o: nemu/src/monitor/hashmap.c
