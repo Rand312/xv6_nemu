0ffc7dbe6869412bfba215d2710bce00ebc88bad
tracer-NEMU2020 <tjuics2020@163.com>
2023-05-30 15:13:57 +0800

    >  compile NEMU 3018216336 rand Linux rand 5.15.0-71-generic #78-Ubuntu SMP Tue Apr 18 09:00:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:13:57 up 12 days,  3:28,  4 users,  load average: 0.78, 1.03, 0.97 9c9f0faa10105ef3b49fc5383e9b07d68a3c7356

--- nemu/obj/kernel/irq/i8259.d
+++
@@ -1,8 +0,0 @@
-obj/kernel/irq/i8259.o: kernel/src/irq/i8259.c kernel/include/x86.h \
- kernel/include/x86/cpu.h lib-common/uclibc/include/stdint.h \
- lib-common/uclibc/include/features.h \
- lib-common/uclibc/include/bits/uClibc_config.h \
- lib-common/uclibc/include/sys/cdefs.h \
- lib-common/uclibc/include/bits/wordsize.h lib-common/x86-inc/cpu.h \
- kernel/include/x86/io.h kernel/include/x86/memory.h \
- lib-common/x86-inc/mmu.h
